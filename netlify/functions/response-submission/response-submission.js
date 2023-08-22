991d75ec6ef1c46132ec279d1a40a3d97cb8c688
Jake Morales <jmora060@ucr.edu>
2023-08-21 22:06:00 -0700

    fix import to get local dev working

--- netlify/functions/response-submission/response-submission.js
+++ netlify/functions/response-submission/response-submission.js
@@ -1,4 +1,4 @@
-import busboy from 'busboy';
+const busboy = require('busboy');
 
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
