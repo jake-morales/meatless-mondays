71565301e679dae896e370d2094993cb326f43bb
Jake Morales <jmora060@ucr.edu>
2023-08-21 20:40:22 -0700

    see if it works

--- netlify/functions/response-submission/response-submission.js
+++ netlify/functions/response-submission/response-submission.js
@@ -1,10 +1,13 @@
+import * as Busboy from 'busboy';
+
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const fields = await parseMultipartForm(event)
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
+      body: JSON.stringify({ message: fields.keys.toString() }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
@@ -15,3 +18,49 @@ const handler = async (event) => {
 }
 
 module.exports = { handler }
+
+// Code copied from https://www.netlify.com/blog/2021/07/29/how-to-process-multipart-form-data-with-a-netlify-function/
+function parseMultipartForm(event) {
+  return new Promise((resolve) => {
+    // we'll store all form fields inside of this
+    const fields = {};
+
+    // let's instantiate our busboy instance!
+    const busboy = new Busboy({
+      // it uses request headers
+      // to extract the form boundary value (the ----WebKitFormBoundary thing)
+      headers: event.headers
+    });
+
+    // before parsing anything, we need to set up some handlers.
+    // whenever busboy comes across a file ...
+    busboy.on(
+      "file",
+      (fieldname, filestream, filename, transferEncoding, mimeType) => {
+        // ... we take a look at the file's data ...
+        filestream.on("data", (data) => {
+          // ... and write the file's name, type and content into `fields`.
+          fields[fieldname] = {
+            filename,
+            type: mimeType,
+            content: data,
+          };
+        });
+      }
+    );
+
+    // whenever busboy comes across a normal field ...
+    busboy.on("field", (fieldName, value) => {
+      // ... we write its value into `fields`.
+      fields[fieldName] = value;
+    });
+
+    // once busboy is finished, we resolve the promise with the resulted fields.
+    busboy.on("finish", () => {
+      resolve(fields)
+    });
+
+    // now that all handlers are set up, we can finally start processing our request!
+    busboy.write(event.body);
+  });
+}
